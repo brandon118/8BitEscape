1072d3851e6dbd9d89d3556e76cf86f539ba01f5
Brandon Moloney <bdm11801@gmail.com>
2019-01-23 16:02:57 -0500

    fixed all problems with the question gen and added some more questions

--- ChallengeGen.js
+++ ChallengeGen.js
@@ -1,27 +1,28 @@
 function ChallengeGen() {
 
-    RngQuestion = Math.round(Math.random() * 10);
-    var QuestionHolder = [
-        Question1 = ["what time is it", "answer1", "answer2", "answer3", "answer4", 2, 1],
-        Question2 = ["why am i doing this", "answer1", "answer2", "answer3", "answer4", 4, 2],
-        Question3 = ["what is the point", "answer1", "answer2", "answer3", "answer4", 1, 3],
-        Question4 = ["this is fine", "answer1", "answer2", "answer3", "answer4", 3, 4],
-        Question5 = ["im not even writing questions now", "answer1", "answer2", "answer3", "answer4", 3, 4],
-        Question6 = ["itll buff", "answer1", "answer2", "answer3", "answer4", 3, 4],
-        Question7 = ["god i hope this works", "answer1", "answer2", "answer3", "answer4", 3, 4],
-        Question8 = ["sick", "answer1", "answer2", "answer3", "answer4", 3, 4],
-        Question9 = ["testing", "answer1", "answer2", "answer3", "answer4", 3, 4],
-        Question10 = ["Question", "answer1", "answer2", "answer3", "answer4", 3, 4],
+    var rand = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+
+    
+    var ArrayOfQuestions = [
+        Question1 = ["Two fathers and two sons sat down to eat eggs for breakfast. They ate exactly three eggs, each person had an egg, how?","One of the 'fathers' is also a grandfather. Therefore the other father is both a son and a father to the grandson."],
+        Question2 = ["Davids father had 3 sons, snap, crackle and who?", "David"],
+        Question3 = ["I have many keys but no locks, what am I?", "A keyboard"],
+        Question4 = ["What is an aliens favorite place on a computer", "The spacebar"],
+        Question5 = ["abc", "answesr1"],
+        Question6 = ["itll buff", "answesaffr1"],
+        Question7 = ["god i hope this works", "answfder1"],
+        Question8 = ["sick", "answaser1"],
+        Question9 = ["testing", "answfger1"],
+        Question10 = ["Question", "answeefr1"],
 
     ];
-    for (var i = 0; i < 4; i++) {
-        if (RngQuestion = QuestionHolder[6]) {
-            console.log(RngQuestion);
-            document.getElementById("chal").innerHTML = QuestionHolder[1];
-        }
-        else {
-            document.getElementById("chal").innerHTML = "that didnt work" + RngQuestion;
-        }
-    }
-    RngQuestion = null;
+    
+    HolderArray = ArrayOfQuestions[rand];
+    
+    document.getElementById("Question").innerHTML = HolderArray[0];
+    document.getElementById("answer").innerHTML = "";
+
+}
+function AnswerGen(){
+    document.getElementById("answer").innerHTML = HolderArray[1];
 }
